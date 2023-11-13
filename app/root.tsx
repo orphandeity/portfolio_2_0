f057779797884446b117a1b4724f2a2ba71861a4
Jeff Williams <88175879+orphandeity@users.noreply.github.com>
2023-11-12 17:35:06 -0800

    adds content

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
+import { cssBundleHref } from '@remix-run/css-bundle'
+import type { LinksFunction } from '@remix-run/node'
 import {
   Links,
   LiveReload,
@@ -7,11 +7,18 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
+} from '@remix-run/react'
+
+import styles from './style.css'
 
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
+  ...(cssBundleHref
+    ? [
+        { rel: 'stylesheet', href: cssBundleHref },
+        { rel: 'stylesheet', href: styles },
+      ]
+    : [{ rel: 'stylesheet', href: styles }]),
+]
 
 export default function App() {
   return (
@@ -23,11 +30,14 @@ export default function App() {
         <Links />
       </head>
       <body>
+        <header>
+          <b>Jeff Williams | Web Developer</b>
+        </header>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
-  );
+  )
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,74 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from '@remix-run/node'
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+    { title: 'New Remix App' },
+    { name: 'description', content: 'Welcome to Remix!' },
+  ]
+}
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="grid gap">
+      <h1>
+        Hi, I'm Jeff...
+        <br />
+        Web Developer
+        <br />
+        and Musician.
+      </h1>
+      <p>
+        I create dynamic web experiences that people love to use. You can read a
+        bit more about me or check out some things I have built.
+      </p>
+
+      <section>
+        <h2>Skills</h2>
+        <details>
+          <summary>Modern Frontend Development</summary>
+          HTML, CSS, JavaScript, TypeScript, React
+        </details>
+        <details>
+          <summary>Server-Side Development</summary>
+          Node, Express, Next, Remix
+        </details>
+        <details>
+          <summary>Database Management</summary>
+          SQL, Postgres, SQLite3
+        </details>
+        <details>
+          <summary>Version Control and Collaboration</summary>
+          Git, GitHub, Figma, Sketch
+        </details>
+      </section>
+
+      <section>
+        <h2>Projects</h2>
+        <ul className="projects">
+          <li>Markdown Editor</li>
+          <li>Galleria Slideshow</li>
+          <li>Planets Fact Site</li>
+        </ul>
+      </section>
+
+      <section>
+        <h2>Contact</h2>
+        <form>
+          <div>
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" />
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" />
+          </div>
+          <div>
+            <label htmlFor="message">Message</label>
+            <textarea id="message" />
+          </div>
+          <button>Send message</button>
+        </form>
+      </section>
     </div>
-  );
+  )
 }

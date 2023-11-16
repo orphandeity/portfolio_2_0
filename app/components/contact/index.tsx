f65bf142e87fb995898b49c890f2fe2f3874dcbc
Jeff Williams <88175879+orphandeity@users.noreply.github.com>
2023-11-15 19:21:09 -0800

    adds contact media query

--- app/components/contact/index.tsx
+++ app/components/contact/index.tsx
@@ -6,25 +6,27 @@ export default function Contact({
   return (
     <section className={styles.contact}>
       <h2>Contact</h2>
-      <p className="text">
-        I'm currently looking for new opportunities. If you'd like to get in
-        touch, please fill out the form below.
-      </p>
-      <form>
-        <div>
-          <label htmlFor="name">Name</label>
-          <input type="text" id="name" />
-        </div>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" />
-        </div>
-        <div>
-          <label htmlFor="message">Message</label>
-          <textarea id="message" />
-        </div>
-        <button className={styles.button}>Send message</button>
-      </form>
+      <div className={styles.container}>
+        <p className="text">
+          I'm currently looking for new opportunities. If you'd like to get in
+          touch, please fill out the form below.
+        </p>
+        <form>
+          <div>
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" />
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" />
+          </div>
+          <div>
+            <label htmlFor="message">Message</label>
+            <textarea id="message" />
+          </div>
+          <button className={styles.button}>Send message</button>
+        </form>
+      </div>
     </section>
   )
 }

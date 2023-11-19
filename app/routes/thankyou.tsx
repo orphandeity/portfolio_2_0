47f64118e68c1a8cae64ae2a4f1a3f816e5d2f1d
Jeff Williams <88175879+orphandeity@users.noreply.github.com>
2023-11-19 13:42:07 -0800

    adds thankyou route

---
+++ app/routes/thankyou.tsx
@@ -0,0 +1,10 @@
+export default function Thankyou(): JSX.Element {
+  return (
+    <div className="thankyou">
+      <p>
+        Thanks for your time and consideration. I will get back to you shortly.
+        ✌️
+      </p>
+    </div>
+  )
+}
